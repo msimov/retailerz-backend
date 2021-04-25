18d11ef8b0df556dc395a1f6e14c3d2a08cbc610
Martin Simov <martinsimov02@gmail.com>
2021-04-26 00:04:54 +0300

    Added fbServiceAccount config file

---
+++ config/fbServiceAccount.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    TYPE: "service_account",
+    PROJECT_ID: "retailerz-dev",
+    PRIVATE_KEY_ID: "695a16b296bba781d991df46897ed787115ba884",
+    PRIVATE_KEY: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCmHOrrfkB8jKef\nfD/igEqWzQEb+94g2WEEukP8UfnUnc37mQ/2hzhJoI3V+6+gIUpCD7d5lUmPZvS+\nOwLN3QZqVUmvWh+pzg6pqpPncZKMhFeMNVyI5DJGK/nHMfk7GCrC/uTsv8Ucvnfm\nwhkR//26Unx1UxlTeT77U7V2kmeOhXP4Cdp8ZgOKm28jmnqf2J0USBw9oprxoG5y\nkXJMQZ/oH9+S4KFkxRAEIGxlnvihbToUAedMrEy00FCvOMqjNUxqBfIT/cKjgxIC\nUNPvCcuKgFYH5Nd5BWnty7z2DRnDMbnT4cGc4ar+AcBqSq+8OD9bxzrONdOZf/A4\nhwTEulsLAgMBAAECggEAE1re49XqmqM4QW9/RsYkVWzemmL0WgcqHvG7ITJwtd2L\nQNZv/eLPOVj78yO9Lh8i4WP7wK7B4EkimZW+TmauAYQdg/vay59xpZhKsoF7uLlk\n4Ave/n5O/fklb0Ml40T+Golg+tWfbdQi8WAf/rEWtc04U41Sr/S6f3CcqLZcsEYx\nH7JXrFPZmL+9Ulr/UEcqDe5H0b+T2FCAO+b7kInjbeXReaQW9MacxRlpeuBe2fwc\nF4SmPBQyagP1yvxZFhZ9ulMgPKa2yPCxw9U5WjjZBtU1+kop7yTp7jmC1v/Eovoq\nhq5ycKhKyDlk81VEbgQNUYPyT/QUbZ/AodGLhjD4GQKBgQDl9zcFsAp/KNun6nXa\nwqJbFvmWwFBr6elcbeYpKxDnhhTKVjB+LoKI6uksrop7suwghWCNO1pm51ydpO6e\n85cSyzhZ7mYRd7MK7AbgOgHYJADhau7HpTVd9gr1SJhiMvSUIBcwmutbI0/JMBf8\nOk3dcP8Kx2zwAu6fZ3JBneAIMwKBgQC46yU49W4WNIvuEF84TYDq0QmO8rd7IRIO\nAnuTWNN6YAYE98DoZb1cUF0HTklzCRoSAoLrcgHiu8cJdolC/ODwlPI3zH8tL1bH\nYMrQiqTBDStDwDrf+MXs5Z4/VoRpyJbahdMCtWxSILjpINAEoh9IzgxMeytTtzyp\nsw3HFq1pyQKBgQCdO3cSW3gB02TEHWmxRgyN7Vp/eURDVG5q9PU+RqTLOAC1C1Mi\noxg7FyqQk1I6G26ZGbiXN/Kqkxve6WsBoRk1D1+bFYYSAV6YZCKbYu3p40v61Aso\n08t2UAjUBmuyEJbrRDm20OWErdMagtiTayXb9ytaKlpQjCCtOqSDOrd3wwKBgQCz\nk8fGFLIf5k+tlUCjUMDNSMtbguIkNO1vpYGUikJ1KSMvV9qC1aqbJ3/wRFqX2FL1\nAnNZapHiOjEEQ5Tw9NsNR9ghz/oBS2Oeo6TR+JIe2EcXuGp0bwbnE+Yl5EMa5613\n8VYF32GeMCDiZPceSMLQ1SikbUiaN+w1cKkEmkU14QKBgQDHNl7YF/8p4SXqCF8I\nn5WkfJ+s2v0xNOX0OlXvOxtXZhDgIiCUN9w+EECj9hLyrntk5czth7iQZJFw0cqE\naVWaz8t1nvPNjT9TyhteKeQgwO1WPERq6k3x8ggkxEiDZF6z+SFAobyiq0rIgpEF\nQP5upPl5MBUJ+LA/AVSEwWyWBQ==\n-----END PRIVATE KEY-----\n",
+    CLIENT_EMAIL: "firebase-adminsdk-5ekq6@retailerz-dev.iam.gserviceaccount.com",
+    CLIENT_ID: "102501913634228998569",
+    AUTH_URI: "https://accounts.google.com/o/oauth2/auth",
+    TOKEN_URI: "https://oauth2.googleapis.com/token",
+    AUTH_PROVIDER_CERT_URL: "https://www.googleapis.com/oauth2/v1/certs",
+    CLIENT_CERT_URL: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5ekq6%40retailerz-dev.iam.gserviceaccount.com"
+}
