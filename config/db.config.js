dd72d544d866658b7e176f1252c79742aaec2d10
Martin Simov <martinsimov02@gmail.com>
2021-06-03 18:50:40 +0300

    Configured cleardb MySQL.

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "retailerz",
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "b2f3cca5386ed6",
+  PASSWORD: "30b3700d",
+  DB: "heroku_ca5712c7beb390a",
 };
